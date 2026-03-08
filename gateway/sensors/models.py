ba8f3e4acdac4a9efb6ae9c9eb5f25559b38252e
Sofya Gomzyakova <gomzyakova.sofya@yandex.ru>
2026-03-08 21:30:51 +0700

    Добавлены читабельные имена в модели датчиков.

--- gateway/sensors/models.py
+++ gateway/sensors/models.py
@@ -3,21 +3,46 @@ from django.utils import timezone
 
 
 class Sensor(models.Model):
-    name = models.CharField(max_length=100)
-    unit = models.CharField(max_length=20)
+    name = models.CharField(
+        'Имя датчика',
+        max_length=100
+    )
+    unit = models.CharField(
+        'Ед. изм.',
+        max_length=20
+    )
+    on = models.BooleanField(
+        'Включен',
+        default=False
+    )
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
 
 
 class SensorData(models.Model):
     sensor = models.ForeignKey(
         Sensor,
         on_delete=models.CASCADE,
-        related_name='data'
+        related_name='data',
+        verbose_name='Сенсор'
     )
-    value = models.FloatField()
-    timestamp = models.DateTimeField(default=timezone.now)
+    value = models.FloatField('Значение')
+    timestamp = models.DateTimeField(
+        'Время измерения',
+        default=timezone.now
+    )
 
     def __str__(self):
         return f'{self.sensor.name}, {self.timestamp}'
+
+    class Meta:
+        get_latest_by = 'timestamp'
+        ordering = ['timestamp']
+        verbose_name = 'Измерение'
+        verbose_name_plural = 'Измерения'
